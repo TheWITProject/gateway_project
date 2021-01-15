d6264234425dc168911c80e75b90370b2cfb5af2
Sam <saparadero@gmail.com>
2021-01-14 20:41:44 -0500

    Task:create models + posts

--- app/models.py
+++ app/models.py
@@ -3,6 +3,10 @@ from app import db
 
 class User(db.Model):
     __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30))
+
+    posts = db.relationship("Post", back_populates="user")
 
     def serialize(self):
         return {}
@@ -10,7 +14,11 @@ class User(db.Model):
 
 class Post(db.Model):
     __tablename__ = "posts"
-
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(280))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    
+    user = db.relationship("User", back_populates="posts")
 
     def serialize(self):
         return {}
