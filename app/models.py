ccc15ff3f0c5bfb3fee8e19c6f7bea10311d4422
Ferduse <ferduse.rahman79@myhunter.cuny.edu>
2021-01-19 19:57:09 -0500

    Updated seed

--- app/models.py
+++ app/models.py
@@ -4,22 +4,22 @@ from app import db
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
+    username = db.Column(db.String(30), nullable=False)
     posts = db.relationship("Post", back_populates="users")
 
     def serialize(self):
         return {
         "id": self.id,
-        "username": self.username,
+        "username": self.user,
         }
 
 
 class Post(db.Model):
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, foreign_key=True)
-    user = db.relationship("User", back_populates="posts")
+    content = db.Column(db.String(280), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    users = db.relationship("User", back_populates="posts")
 
     def serialize(self):
         return {
--- app/seed.py
+++ app/seed.py
@@ -3,29 +3,29 @@ import datetime
 
 def seed(db):
     users = [
-        User(id=1, name="Zac Efron"),
-        User(id=2, name="Rock Lee"),
-        User(id=3, name="Casey Leur"),
-        User(id=4, name="Jasmine Keen"),
-        User(id=5, name="John Smith"),
+        User(id=1, username="Zac Efron"),
+        User(id=2, username="Rock Lee"),
+        User(id=3, username="Casey Leur"),
+        User(id=4, username="Jasmine Keen"),
+        User(id=5, username="John Smith"),
     ]
 
     posts = [
-        Post(id=1, posts="Hey!", completed=False, user_id=1),
-        Post(id=2, posts="Enjoying life", completed=False, user_id=1),
-        Post(id=3, posts="Nonstop coding", completed=False, user_id=3),
-        Post(id=4, posts="The grind never stops", completed=False, user_id=3),
-        Post(id=5, posts="I think my cats hiding from me", completed=False, user_id=2),
-        Post(id=6, posts="On my way to Six Flags!!!", completed=False, user_id=2),
-        Post(id=7, posts="i love tea more than coffee", completed=False, user_id=5),
-        Post(id=8, posts="Learning a new language, brb", completed=False, user_id=3),
-        Post(id=9, posts="yup, i love sushi.", completed=False, user_id=2),
-        Post(id=10, posts="I am proud of myself!", completed=False, user_id=2),
-        Post(id=11, posts="omw to cali", completed=False, user_id=4),
-        Post(id=12, posts="i daydream too much lol", completed=False, user_id=4),
-        Post(id=13, posts="kinda tired...", completed=False, user_id=1),
-        Post(id=14, posts="i should start sleeping earlier ;/", completed=False, user_id=3),
-        Post(id=15, posts="i forgot my tweet,, oops", completed=False, user_id=5),
+        Post(id=1, content="Hey!", user_id=1),
+        Post(id=2, content="Enjoying life", user_id=1),
+        Post(id=3, content="Nonstop coding", user_id=3),
+        Post(id=4, content="The grind never stops", user_id=3),
+        Post(id=5, content="I think my cats hiding from me", user_id=2),
+        Post(id=6, content="On my way to Six Flags!!!", user_id=2),
+        Post(id=7, content="i love tea more than coffee", user_id=5),
+        Post(id=8, content="Learning a new language, brb", user_id=3),
+        Post(id=9, content="yup, i love sushi.", user_id=2),
+        Post(id=10, content="I am proud of myself!", user_id=2),
+        Post(id=11, content="omw to cali", user_id=4),
+        Post(id=12, content="i daydream too much lol",user_id=4),
+        Post(id=13, content="kinda tired...", user_id=1),
+        Post(id=14, content="i should start sleeping earlier ;/", user_id=3),
+        Post(id=15, content="i forgot my tweet,, oops", user_id=5),
     ]
 
     db.session.add_all(users)
