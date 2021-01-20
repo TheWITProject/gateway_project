27941ba2920bc975b1f01dab93ef0198ee2e8465
AuditeTalukder <ashatalukder@gmail.com>
2021-01-19 19:51:26 -0500

    create and input tables/rows

--- app/models.py
+++ app/models.py
@@ -3,14 +3,28 @@ from app import db
 
 class User(db.Model):
     __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable= False) 
+    posts = db.relationship("Post", back_populates="users")
+    
 
     def serialize(self):
-        return {}
+        return {
+            "id": self.id,
+            "username": self.username
+        }
 
 
 class Post(db.Model):
     __tablename__ = "posts"
-
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(280), nullable=False) 
+    users_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    users = db.relationship("User", back_populates="posts")
 
     def serialize(self):
-        return {}
+        return {
+            "id": self.id,
+            "content": self.content,
+            "users_id":self.users_id
+        }
--- app/seed.py
+++ app/seed.py
@@ -3,10 +3,24 @@ import datetime
 
 def seed(db):
     users = [
-
+        User(id=1, username="audite.t"),
+        User(id=2, username="spongebob.squarepants"),
+        User(id=3, username="patrick.star"),
+        User(id=4, username="squirward.tentacles"),
+        User(id=5, username="mr.krabs"),
     ]
 
     posts = [
+        Post(id=1, content="I Ain't"),
+        Post(id=2, content="Never Seen"),
+        Post(id=3, content="Two Pretty"),
+        Post(id=4, content="Best"),
+        Post(id=5, content="Friends"),
+        Post(id=6, content="Let's learn about the F.U.N. song!"),
+        Post(id=7, content="F is for friends who do stuff together"),
+        Post(id=8, content="U is for you and me"),
+        Post(id=9, content="N is for anywhere and anytime at all"),
+        Post(id=10, content="Down here in the deep blue sea!"),
 
     ]
 
