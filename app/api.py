89c44e8337abe2fb97dacdf621f6eac9b4a965b2
Sam <saparadero@gmail.com>
2021-01-19 20:52:00 -0500

    Task:start working on index.html

--- app/api.py
+++ app/api.py
@@ -2,3 +2,7 @@ from flask import request, render_template, redirect
 from app import app, db
 from models import User, Post
 
+# GET /
+@app.route('/', methods=['GET'])
+def get_index():
+    return render_template('index.html')
