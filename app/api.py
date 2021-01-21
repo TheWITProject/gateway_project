ca6e38908f9662be09c0a229ea2ccd5c81d71acb
Jungah Ahn <jungahahn@Jungahs-MacBook-Pro.local>
2021-01-21 18:57:39 -0500

    creating template to display all posts

--- app/api.py
+++ app/api.py
@@ -2,3 +2,13 @@ from flask import request, render_template, redirect
 from app import app, db
 from models import User, Post
 
+# ------------------------------------------------
+# HOME ROUTES
+# ------------------------------------------------
+
+# GET /
+@app.route('/', methods=['GET'])
+def get_home():
+    data = Post.query.all()
+    all_posts = [item.serialize() for item in data]
+    return render_template('index.html', posts=all_posts)
