b6e509520ce500a13893790ef6718f891f716ce0
angelouis <alouis001@citymail.cuny.edu>
2021-01-21 20:40:17 -0500

    started website

--- app/api.py
+++ app/api.py
@@ -2,3 +2,15 @@ from flask import request, render_template, redirect
 from app import app, db
 from models import User, Post
 
+
+#-----------------------------------------------
+# POST ROUTES 
+#-----------------------------------------------
+
+# GET /posts
+@app.route('/posts', methods=['GET'])
+def all_posts():
+    data = Post.query.all()
+    all_posts = [item.serialize() for item in data]
+    return render_template('index.html', posts=all_posts)
+
