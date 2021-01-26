851803312a2e8baacfdae1020682a471853ef1c2
suhaimaislam <73371083+suhaimaislam@users.noreply.github.com>
2021-01-25 19:26:13 -0500

    Successful Commit

--- app/api.py
+++ app/api.py
@@ -1,4 +1,14 @@
-from flask import request, render_template, redirect
+from flask import request, render_template, redirect, jsonify
 from app import app, db
 from models import User, Post
 
+
+# @app.route('/', methods=['GET'])
+# def get_home():
+#     return render_template('index.html')
+
+@app.route('/', methods=['GET'])
+def get_home():
+    data = Post.query.all()
+    all_posts = [item.serialize() for item in data]
+    return render_template('index.html', posts=all_posts)
