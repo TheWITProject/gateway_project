dfe525e139bc274abc5aeda4511e406ee1c7a8a1
Sam <saparadero@gmail.com>
2021-01-26 20:37:53 -0500

    Task:View posts by user

--- app/api.py
+++ app/api.py
@@ -8,3 +8,10 @@ def get_home():
     data = Post.query.all()
     all_posts = [item.serialize() for item in data]
     return render_template('index.html', posts=all_posts)
+
+# GET /users_posts
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_one_user(user_id):
+    data = User.query.get(user_id)
+    one_user = data.serialize()
+    return render_template('user_posts.html', users=[one_user])
