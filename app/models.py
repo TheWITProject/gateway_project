2f90addb097a10a07452d16a544c20911f3cda44
suhaimaislam <73371083+suhaimaislam@users.noreply.github.com>
2021-01-26 19:45:40 -0500

    All Posts by User Page

--- app/models.py
+++ app/models.py
@@ -4,13 +4,13 @@ from app import db
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, nullable=False)
+    username = db.Column(db.String(30), nullable=False)
     posts = db.relationship("Post", back_populates='user')
     
     def serialize(self):
         return {
             "id": self.id,
-            "name": self.name,
+            "username": self.username,
             "posts": [item.serialize() for item in self.posts]
         }
 
@@ -18,7 +18,7 @@ class User(db.Model):
 class Post(db.Model):
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String, nullable=False)
+    content = db.Column(db.String(280), nullable=False)
     
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     user = db.relationship("User", back_populates="posts")
